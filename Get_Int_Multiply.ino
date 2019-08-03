1af07d7e28dbe7cea3ff82ab1e625086f4a150da
Arunn Balasundram <vbarunn@gmail.com>
2019-08-03 11:54:50 +0530

    Add new files

---
+++ Get_Int_Multiply.ino
@@ -0,0 +1,42 @@
+
+int a;
+int b;
+int results;
+// the setup function runs once when you press reset or power the board
+void setup() {
+Serial.begin(9600);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+
+  while(!Serial.available()){
+    delay(100);
+    
+    }
+
+
+  Serial.println("Enter first number :");
+  delay(5000);
+  a=Serial.parseInt();
+
+  Serial.println("Enter 2nd number :");
+  delay(5000);
+  b=Serial.parseInt();
+  
+  results= multi(a,b);                  // wait for a second
+  Serial.print("Multiplication is : ");
+  Serial.println(results);
+  delay(5000);
+}
+
+
+int multi(int x,int y){
+int z=x*y;
+ return z; 
+}
+
+
+
+
+
---
+++ Split_String_Serial.ino
@@ -0,0 +1,23 @@
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+//*johnjgjjhhnn#20$168.9
+while(Serial.available()){
+String data=Serial.readString();
+
+String name1=data.substring(data.indexOf("*")+1,data.indexOf("#"));
+String age=data.substring(data.indexOf("#")+1,data.indexOf("$"));
+String height=data.substring(data.indexOf("$")+1);
+
+String printdata= "Name : "+name1+"Age : "+age+"Height : " +height;
+Serial.println(printdata);
+
+  
+}
+
+delay(1000);
+}
